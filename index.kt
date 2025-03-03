32ba6db7cd6bcfbc24d6e8adf4702657806104e0
Baidaidai_GFWD <1302064396@qq.com>
2025-03-03 14:30:55 +0800

    added index.kt

--- index.kt
+++ index.kt
@@ -1,4 +1,14 @@
-fun main(){
-    var aa = "aaa"
-    println(aa)
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`);
+
+    print("请输入您的姓名：");
+    val name = scanner.next();
+
+    print("请输入您的年龄：");
+    val age = scanner.next();
+
+    println("您的姓名是：$name，年龄是：$age");
+    scanner.close();
 }
